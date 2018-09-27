f0379cdfd2af06f94b07dc4b68f3be155130eba9
Sokol V <1470715@gmail.com>
2018-09-27 12:51:36 +0300

    check coincidence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketString = bracketsConfig.map(function(item){
+    return item.join('');
+  });
+  let wasChanged = true;
+
+  while(str.length && wasChanged){
+    wasChanged = false;
+    for(let i = 0, len = bracketString.length; i < len; i++){
+      let pos = str.indexOf(bracketString[i]);
+
+      if(pos != -1){
+        str = str.slice(0 , pos) + str.slice(pos + 2);
+        wasChanged = true;
+      }
+    }
+  }
+  return str.length ? false : true;
 }
